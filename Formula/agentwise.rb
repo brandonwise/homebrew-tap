0c8e9a167413e7931b84ae6fdf40a5f399639acf
Brandon <bwise@orbit.bwise.local>
2026-03-16 10:10:29 -0400

    Update agentwise to v0.1.2

--- Formula/agentwise.rb
+++ Formula/agentwise.rb
@@ -1,26 +1,26 @@
 class Agentwise < Formula
   desc "A fast, offline security scanner for MCP server configurations"
   homepage "https://github.com/brandonwise/agentwise"
-  version "0.1.1"
+  version "0.1.2"
   license any_of: ["MIT", "Apache-2.0"]
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/brandonwise/agentwise/releases/download/v0.1.1/agentwise-0.1.1-macos-arm64.tar.gz"
-      sha256 "c41b665e5cb3575dc66fc6c5ae6aa56c90782051921430e2ac60564462ee9d9b"
+      url "https://github.com/brandonwise/agentwise/releases/download/v0.1.2/agentwise-0.1.2-macos-arm64.tar.gz"
+      sha256 "d4b0e690e9ee9471e7267e286ec8b2d9287cd134ddf53ab240283c8bdc18ee67"
     else
-      url "https://github.com/brandonwise/agentwise/releases/download/v0.1.1/agentwise-0.1.1-macos-amd64.tar.gz"
-      sha256 "aca6653dbb1c1b6f0e3415c8ff08f55b9cc2801182b3635b71a4a3538c22867d"
+      url "https://github.com/brandonwise/agentwise/releases/download/v0.1.2/agentwise-0.1.2-macos-amd64.tar.gz"
+      sha256 "c81dfc8eec3317c40e607c10b4e4fa538d8cf788c1cf1136e3571622ab027a6a"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm?
-      url "https://github.com/brandonwise/agentwise/releases/download/v0.1.1/agentwise-0.1.1-linux-arm64.tar.gz"
-      sha256 "6dc9cdc0142427b1034fc782f890265c15ac561680bb0447d1d4ef91e41df1f8"
+      url "https://github.com/brandonwise/agentwise/releases/download/v0.1.2/agentwise-0.1.2-linux-arm64.tar.gz"
+      sha256 "a51befa9ced5323a368249a8525fe615d0ae3d7d2e1753b495b00392af7b8216"
     else
-      url "https://github.com/brandonwise/agentwise/releases/download/v0.1.1/agentwise-0.1.1-linux-amd64.tar.gz"
-      sha256 "50d2c67bb16018cbee6dac8bd00a3800f6880beac7fbfcdf1c3d5e2d657c271c"
+      url "https://github.com/brandonwise/agentwise/releases/download/v0.1.2/agentwise-0.1.2-linux-amd64.tar.gz"
+      sha256 "769baf812854576da6159c403b6644dac3d333df0da69ad305cb100eeb584840"
     end
   end
 

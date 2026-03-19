c8053036e4e5b97a2833a7fc8c7f27cc08b73845
Brandon <bwise@orbit.bwise.local>
2026-03-18 21:40:40 -0400

    agentwise 0.1.3

--- Formula/agentwise.rb
+++ Formula/agentwise.rb
@@ -1,26 +1,26 @@
 class Agentwise < Formula
   desc "A fast, offline security scanner for MCP server configurations"
   homepage "https://github.com/brandonwise/agentwise"
-  version "0.1.2"
-  license any_of: ["MIT", "Apache-2.0"]
+  version "0.1.3"
+  license "MIT"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/brandonwise/agentwise/releases/download/v0.1.2/agentwise-0.1.2-macos-arm64.tar.gz"
-      sha256 "d4b0e690e9ee9471e7267e286ec8b2d9287cd134ddf53ab240283c8bdc18ee67"
+      url "https://github.com/brandonwise/agentwise/releases/download/v0.1.3/agentwise-0.1.3-macos-arm64.tar.gz"
+      sha256 "c9455e89788ead546992a031dcdfcd10418cce2a9dd896ccb315ff7d04be9771"
     else
-      url "https://github.com/brandonwise/agentwise/releases/download/v0.1.2/agentwise-0.1.2-macos-amd64.tar.gz"
-      sha256 "c81dfc8eec3317c40e607c10b4e4fa538d8cf788c1cf1136e3571622ab027a6a"
+      url "https://github.com/brandonwise/agentwise/releases/download/v0.1.3/agentwise-0.1.3-macos-amd64.tar.gz"
+      sha256 "f9857e2fe65820d40dee939757a9476dcbcb055e936aa2fdfc754f788923b5bb"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm?
-      url "https://github.com/brandonwise/agentwise/releases/download/v0.1.2/agentwise-0.1.2-linux-arm64.tar.gz"
-      sha256 "a51befa9ced5323a368249a8525fe615d0ae3d7d2e1753b495b00392af7b8216"
+      url "https://github.com/brandonwise/agentwise/releases/download/v0.1.3/agentwise-0.1.3-linux-arm64.tar.gz"
+      sha256 "c928236c7e470c861944cee9f2aca1880f631b097d93fe48c6488b1b889b0e14"
     else
-      url "https://github.com/brandonwise/agentwise/releases/download/v0.1.2/agentwise-0.1.2-linux-amd64.tar.gz"
-      sha256 "769baf812854576da6159c403b6644dac3d333df0da69ad305cb100eeb584840"
+      url "https://github.com/brandonwise/agentwise/releases/download/v0.1.3/agentwise-0.1.3-linux-amd64.tar.gz"
+      sha256 "857022e150b7526d9ca16da9888c6ddbe063e39e43c69f5f14285893abeba649"
     end
   end
 
